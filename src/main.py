71f95b793956aba62c66dfc89a0e75a9c79d3716
Mike Lu <heatmint2333@gmail.com>
2018-09-16 16:31:21 +0800

    initial coding!

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file("static/form.html")
+
+@app.route('/api',methods=["POST"])
+def process():
+    print request.form.get("size")
+    #todo logic here
+    return redirect("/thanks")
+
+@app.route('/thanks')
+def thanks():
+    return send_file("static/thanks.html")
+
+app.run(
+    port=80,
+    debug=True
+)
